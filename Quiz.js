ee89a85b3d5cd82ff0b9998f01085592c4a832f2
Zyten <sruban707@hotmail.com>
2023-04-09 06:20:27 +0800

    Improve UI and UX

--- Quiz.js
+++ Quiz.js
@@ -1,8 +1,9 @@
 // Component for displaying a quiz question
 class QuizQuestion {
-  constructor(questionData, onAnswerSelected) {
+  constructor(questionData, onAnswerSelected, onQuestionFinished) {
     this.questionData = questionData;
     this.onAnswerSelected = onAnswerSelected;
+    this.onQuestionFinished = onQuestionFinished;
     this.element = this.createQuestionElement();
     this.videoPlayer = null;
     this.selectedAnswer = null;
@@ -10,44 +11,104 @@ class QuizQuestion {
 
   createQuestionElement() {
     const container = document.createElement("div");
-    container.classList.add("quiz-question");
+    container.classList.add("quiz-question", "my-8");
 
     const title = document.createElement("h2");
     title.innerText = this.questionData.title;
+    title.classList.add("text-xl", "font-medium", "mb-4");
     container.appendChild(title);
 
     const playerContainer = document.createElement("div");
-    playerContainer.classList.add("player-container");
+    playerContainer.classList.add(
+      "player-container",
+      "mb-4",
+      "relative",
+      "h-72",
+      "w-full",
+      "pb-3/4"
+    );
     container.appendChild(playerContainer);
 
+    const answerButtonsContainer = document.createElement("div");
+    answerButtonsContainer.classList.add(
+      "answer-buttons-container",
+      "flex",
+      "flex-wrap",
+      "justify-center",
+      "mb-4"
+    );
+    container.appendChild(answerButtonsContainer);
+
     const answerButtons = document.createElement("div");
-    answerButtons.classList.add("answer-buttons");
-    container.appendChild(answerButtons);
+    answerButtons.classList.add(
+      "answer-buttons",
+      "flex",
+      "flex-wrap",
+      "justify-center"
+    );
+    answerButtonsContainer.appendChild(answerButtons);
 
     this.questionData.options.forEach((option) => {
       const button = document.createElement("button");
       button.innerText = option;
-      button.classList.add("btn");
+      button.classList.add(
+        "btn",
+        "bg-neutral-500",
+        "hover:bg-neutral-700",
+        "text-white",
+        "font-bold",
+        "m-2",
+        "py-2",
+        "px-4",
+        "rounded"
+      );
       button.addEventListener("click", () => {
         if (this.selectedAnswer === null) {
           this.selectedAnswer = option;
-          button.classList.add("selected");
+          event.target.classList.add("selected");
+          if (option === this.questionData.correctOption) {
+            event.target.classList.add("bg-green-500");
+          } else {
+            event.target.classList.add("bg-red-500");
+          }
           this.onAnswerSelected(option === this.questionData.correctOption);
+
+          document
+            .querySelectorAll(".answer-buttons > button")
+            .forEach((btn) => btn.classList.remove("hover:bg-neutral-700"));
         }
       });
       answerButtons.appendChild(button);
     });
 
+    // Add the Next button
+    const nextButton = document.createElement("button");
+    nextButton.innerText = "Next";
+    nextButton.classList.add(
+      "btn-next",
+      "hidden",
+      "bg-blue-500",
+      "hover:bg-blue-700",
+      "text-white",
+      "p-3",
+      "block",
+      "w-full"
+    );
+    nextButton.addEventListener("click", () => {
+      this.onQuestionFinished();
+    });
+    container.appendChild(nextButton);
+
     // Initialize the YouTube video player
     this.videoPlayer = new YT.Player(playerContainer, {
-      height: "360",
-      width: "640",
+      height: "100%",
+      width: "100%",
       videoId: this.questionData.videoId,
       playerVars: {
-        origin: "http://127.0.0.1:5500",
+        origin: location.origin,
         autoplay: 1,
-        controls: 0,
-        disablekb: 0,
+        controls: 1,
+        disablekb: 1,
         iv_load_policy: 3,
         modestbranding: 1,
         playsinline: 1,
@@ -72,6 +133,7 @@ class QuizQuestion {
 }
 
 // Component for managing the quiz and displaying the questions
+// Component for managing the quiz and displaying the questions
 class Quiz {
   constructor(quizData) {
     this.quizData = quizData;
@@ -83,21 +145,9 @@ class Quiz {
     const container = document.createElement("div");
     container.classList.add("quiz");
 
-    const nextButton = document.createElement("button");
-    nextButton.innerText = "Next";
-    nextButton.addEventListener("click", () => {
-      this.currentIndex++;
-      if (this.currentIndex >= this.quizData.length) {
-        this.onQuizFinished();
-      } else {
-        this.showQuestion();
-      }
-    });
-    container.appendChild(nextButton);
-
     // Add a container for the question element
     const questionContainer = document.createElement("div");
-    questionContainer.classList.add("question-container");
+    questionContainer.classList.add("question-container", "mt-5");
     container.appendChild(questionContainer);
 
     return container;
@@ -128,14 +178,8 @@ class Quiz {
     );
     answerButtons.forEach((button) => (button.disabled = true));
 
-    // Show feedback
-    const feedback = document.createElement("p");
-    feedback.innerText = isCorrect ? "Correct!" : "Incorrect";
-    feedback.classList.add("feedback");
-    questionElement.appendChild(feedback);
-
     // Show the next button
-    const nextButton = this.element.querySelector("button");
+    const nextButton = this.element.querySelector(".btn-next");
     nextButton.classList.remove("hidden");
 
     // Play the reveal animation after a short delay
