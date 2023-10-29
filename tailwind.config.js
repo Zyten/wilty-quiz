44b06b70d69afd86b94e2c2149d76d01668f529e
Zyten <sruban707@hotmail.com>
2023-10-30 00:23:12 +0800

    Fix deprecated syntax in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 // Setup Tailwind with postcss: https://v2.tailwindcss.com/docs/guides/vue-3-vite
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'media',
   theme: {
     extend: {},
