38e083d77a4047caa33da5bf01d185db53d96c14
Zyten <sruban707@hotmail.com>
2023-10-30 00:21:47 +0800

    Add gh-pages package to deploy Vue in Github Pages
    Works locally. npm run deploy will build the dist folder then gh-pages will push dist to gh-pages branch

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+  ? '/wilty-quiz/'
+  : '/wilty-quiz/',
   plugins: [
     vue(),
   ],
